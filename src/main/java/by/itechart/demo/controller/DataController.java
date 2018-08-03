2f1b606a602ed6077db7a3c14532e74728d918e5
gennady.novik <Gennaidy.Novik@Softensity>
2018-08-03 13:29:48 +0300

    continue

---
+++ src/main/java/by/itechart/demo/controller/DataController.java
@@ -0,0 +1,7 @@
+package by.itechart.demo.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DataController {
+}
--- src/main/java/by/itechart/demo/controller/TemplateController.java
+++ src/main/java/by/itechart/demo/controller/TemplateController.java
@@ -2,23 +2,25 @@ package by.itechart.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import java.util.Map;
 
 @Controller
 public class TemplateController {
 
     private final Logger log = LoggerFactory.getLogger(TemplateController.class);
 
-    @GetMapping("/welcome")
+    @GetMapping("/home")
     public String index() {
 
         log.debug("index");
 
-        return "index";
+        return "index.html";
+    }
+
+    @GetMapping("/students")
+    public String students() {
+        return "students.html";
     }
 }
